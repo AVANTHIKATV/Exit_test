eee9fbb11dbf6110a946f9cfeef26bee45ea07c4
AVANTHIKATV <avanthikakyn@gmail.com>
2025-02-03 15:04:16 +0530

    index.js

--- monsoonExit-main/monsoonExit-main/Backend/index.js
+++ monsoonExit-main/monsoonExit-main/Backend/index.js
@@ -1,32 +1,37 @@
 const express = require("express");
-require("./connection")
-const BlogModel=require("./model")
 const cors = require("cors");
+require("./connection.js"); 
+const BlogModel = require("./model.js");
 
 const app = express();
-var PORT = 3001;
+const PORT = 3001;
+
 app.use(express.json());
 app.use(cors());
 
+
 app.post("/add", async (req, res) => {
   try {
-      await BlogModel(req.body).save()
-      res.send({message:"Blog added"})
+    const newBlog = new BlogModel(req.body);
+    await newBlog.save();
+    res.status(201).json({ message: "Data added successfully!" });
   } catch (error) {
-      console.log(error)
+    console.error("Error adding data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 
 app.get("/get", async (req, res) => {
   try {
-    let data = await BlogModel.find();
-    res.send(data);
+    const data = await BlogModel.find();
+    res.status(200).json(data);
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
 app.listen(PORT, () => {
-  console.log(`${PORT} is up and running`);
+  console.log(`Server is running on port ${PORT}`);
 });
